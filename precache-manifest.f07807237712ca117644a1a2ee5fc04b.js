ccbd28a0504ba72e8b3fff5da9b1fdb0bc675897
Sutachart <sutachart_me@kkumail.com>
2020-08-28 14:20:07 +0600

    Updates

---
+++ precache-manifest.f07807237712ca117644a1a2ee5fc04b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee12aa8bc3f558008712b8e33854cc08",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "22b30897a6ada9ce5e0f",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1cf1f66540475e5b52b5",
+    "url": "/portfolio/static/js/2.cdb902ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.cdb902ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22b30897a6ada9ce5e0f",
+    "url": "/portfolio/static/js/main.9e550cb5.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
